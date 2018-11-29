2da14746b5d80b6e6e4cac00aaf3bd451b190a1b
Reniferek <tantum2@wp.pl>
2018-11-30 00:56:52 +0100

    Added AccountTest and BankTest, they don't work tho

---
+++ JtestLab/test/b_Money/AccountTest.java
@@ -0,0 +1,54 @@
+package b_Money;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AccountTest {
+	Currency SEK, DKK;
+	Bank Nordea;
+	Bank DanskeBank;
+	Bank SweBank;
+	Account testAccount;
+	
+	@Before
+	public void setUp() throws Exception {
+		SEK = new Currency("SEK", 0.15);
+		SweBank = new Bank("SweBank", SEK);
+		SweBank.openAccount("Alice");
+		testAccount = new Account("Hans", SEK);
+		testAccount.deposit(new Money(10000000, SEK));
+
+		SweBank.deposit("Alice", new Money(1000000, SEK));
+	}
+	
+	@Test
+	public void testAddRemoveTimedPayment() {
+		testAccount.addTimedPayment("1", 5, 10, new Money(150, SEK), SweBank, "Alice");
+		assertTrue(testAccount.timedPaymentExists("1"));
+		testAccount.removeTimedPayment("1");
+		assertFalse(testAccount.timedPaymentExists("0"));
+	}
+	
+	@Test
+	public void testTimedPayment() throws AccountDoesNotExistException {
+		testAccount.addTimedPayment("1", 1, 2, new Money(1, SEK), SweBank, "Alice"); 
+		System.out.println(testAccount.getBalance().getAmount());
+		testAccount.tick();
+		testAccount.tick();
+		assertEquals(new Integer(10000000 - 150 - 1), testAccount.getBalance().getAmount(), 0);
+	}
+
+	@Test
+	public void testAddWithdraw() {
+		testAccount.deposit(new Money(5000, SEK));
+		assertEquals(10000000 - 150 + 5000, testAccount.getBalance().getAmount(), 0);  
+		testAccount.withdraw(new Money(5000, SEK));
+		assertEquals(10000000, testAccount.getBalance().getAmount(), 0);
+	}
+	
+	@Test
+	public void testGetBalance() {
+		assertTrue(new Money(10000000 - 150, SEK).equals(testAccount.getBalance()));
+	}
+}
